48361e54a4bf0b1897409e29d44863c1e947440a
Justin Burrill <me@justinburrill.ca>
2025-03-23 15:04:08 -0400

    mar23/2025

--- init.lua
+++ init.lua
@@ -11,3 +11,10 @@ vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
 vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
 vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+
+vim.opt.clipboard = "unnamedplus"
+
+-- for deno lsp?
+vim.g.markdown_fenced_languages = {
+  "ts=typescript",
+}
