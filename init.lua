d19d4a44ef6f7ec8bae4f7e843eb6ed6b54573eb
Justin Burrill <me@justinburrill.ca>
2025-09-12 00:10:56 -0400

    update Fri Sep 12 00:10:56 EDT 2025

--- init.lua
+++ init.lua
@@ -22,6 +22,7 @@ vim.g.clipboard = {
     cache_enabled = 0,
 }
 
+-- PLUGINS PACKAGES
 vim.pack.add({
     { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
     { src = "https://github.com/vague2k/vague.nvim",             name = "vague" },
@@ -34,6 +35,7 @@ vim.pack.add({
     { src = "https://github.com/Jari27/lazydev.nvim",            name = "lazydev", ft = "lua", version = "deprecate_client_notify" },
     { src = "https://github.com/mason-org/mason.nvim" },
     { src = "https://github.com/mason-org/mason-lspconfig.nvim" },
+    { src = "https://github.com/folke/which-key.nvim" },
 })
 
 local lspconfig = require("lspconfig")
@@ -45,8 +47,57 @@ require "nvim-treesitter".setup()
 require "oil".setup()
 require "mason".setup()
 require "mason-lspconfig".setup()
+require "which-key".setup({
+    notify = true,
+    preset = "helix",
+    delay = 300,
+    plugins = {
+        marks = true,
+        registers = true,
+    },
+    win = {
+        width = { max = 100 },
+        no_overlap = false,
+        height = { min = 10, max = 30 },
+        wo = {
+            winblend = 1,
+        }
+    },
+    layout = {
+        width = { max = 50 }
+    }
+})
+require "vague".setup({
+    style = {
+        strings = "none",
+        keywords = "bold",
+    },
+})
 
-vim.lsp.enable({ "lua_ls", "clangd", "basedpyright", "rust_analyzer", "denols", "ts_ls" })
+-- TREESITTER
+local treesitter_ok, treesitter_configs = pcall(require, "nvim-treesitter.configs")
+if treesitter_ok then
+    treesitter_configs.setup {
+        ensure_installed = { "c", "cpp", "lua", "python", "typescript",
+            "vim", "rust", "vue", "sql", "html", "css" },
+        sync_install = true,
+        ignore_install = {},
+        auto_install = true,
+        highlight = {
+            enable = true,
+        },
+        modules = {},
+        rainbow = {
+            enable = true,
+            extended_mode = true,
+            max_file_lines = 10000,
+            -- colors = {},
+        },
+    }
+end
+
+-- LSPCONFIG
+vim.lsp.enable({ "lua_ls", "clangd", "basedpyright", "rust_analyzer", "denols", "ts_ls", "black" })
 vim.lsp.config("basedpyright", {
     settings = {
         basedpyright = {
@@ -73,32 +124,39 @@ vim.lsp.config("ts_ls", {
     single_file_support = false
 })
 
+-- tell autocomplete about neovim lsp completion
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(ev)
+        local client = vim.lsp.get_client_by_id(ev.data.client_id)
+        if client ~= nil and client:supports_method("textDocument/completion") then
+            vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = false })
+        end
+    end,
+})
+
+-- KEYBINDS KEYMAPS
 local commentapi = require("Comment.api")
 local escape_key = vim.api.nvim_replace_termcodes("<ESC>", true, false, true)
-
-
-require("vague").setup({
-    style = {
-        strings = "none",
-        keywords = "bold",
-    },
-})
-vim.cmd("colorscheme vague")
-
-
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = "format" })
 vim.keymap.set("n", "<C-Space>", vim.lsp.buf.hover)
-vim.keymap.set("i", "<C-Space>", "<C-x><C-o>")
+vim.keymap.set("i", "<C-Space>", "<C-x><C-o>") -- omnifunc autocomplete
+vim.keymap.set("i", "<C-z>", function()
+    -- TODO make this delete something i just pasted from "* or "+ with C-v
+    -- if the text before my cursor isn't from the clipboard, then delete
+    -- until punctuation or x many characters or something
+end)
 vim.keymap.set("n", "<leader>e", ":Oil<CR>")
-vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename)
-vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
-vim.keymap.set("n", "<leader>o", ":Pick files<CR>")
-vim.keymap.set("n", "<leader>h", ":Pick help<CR>")
-vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float)
-vim.keymap.set("n", "[d", function() vim.diagnostic.jump({ count = -1, float = true }) end) -- jump to diagnostic
-vim.keymap.set("n", "]d", function() vim.diagnostic.jump({ count = 1, float = true }) end)
-vim.keymap.set("i", "<C-H>", "<C-W>")                                                       -- delete word with ctrl+backspace
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "rename" })
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
+vim.keymap.set("n", "<leader>o", ":Pick files<CR>", { desc = ":Pick files" })
+vim.keymap.set("n", "<leader>h", ":Pick help<CR>", { desc = ":Pick help" })
+vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "View diagnostic" })
+vim.keymap.set("n", "[d", function() vim.diagnostic.jump({ count = -1, float = true }) end,
+    { desc = "Jump to previous diagnostic" })
+vim.keymap.set("n", "]d", function() vim.diagnostic.jump({ count = 1, float = true }) end,
+    { desc = "Jump to next diagnostic" })
+vim.keymap.set("i", "<C-H>", "<C-W>") -- delete word with ctrl+backspace
 
 -- linewise comment
 vim.keymap.set({ "i", "n" }, "<C-_>", function() commentapi.toggle.linewise.current() end)
@@ -113,12 +171,7 @@ vim.keymap.set("x", "<M-/>", function()
     commentapi.toggle.blockwise(vim.fn.visualmode())
 end)
 
--- tell autocomplete about neovim lsp completion
-vim.api.nvim_create_autocmd("LspAttach", {
-    callback = function(ev)
-        local client = vim.lsp.get_client_by_id(ev.data.client_id)
-        if client ~= nil and client:supports_method("textDocument/completion") then
-            vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = false })
-        end
-    end,
-})
+
+-- ACTIVATE COLOURSCHEME
+vim.cmd("colorscheme vague")
+
