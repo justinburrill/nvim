f1b71310ab522677991c9de02d9d95ef4a2147c9
Justin Burrill <me@justinburrill.ca>
2025-12-26 01:27:14 -0500

    update Fri Dec 26 01:27:14 EST 2025

---
+++ lua/lspsettings.lua
@@ -0,0 +1,84 @@
+
+require "utils"
+local lspconfig = require("lspconfig")
+vim.lsp.enable({ "lua_ls", "clangd", "basedpyright", "rust_analyzer", "ts_ls", "bashls", "jsonls", "vue_ls" })
+vim.lsp.config("basedpyright", {
+    settings = {
+        basedpyright = {
+            analysis = {
+                diagnosticSeverityOverrides = {
+                    reportUnknownParameterType = false,
+                    reportUnknownVariableType = false,
+                    reportUnknownLambdaType = false,
+                    reportUnknownMemberType = false,
+                    reportUnknownArgumentType = false,
+                    reportMissingParameterType = false,
+                    reportUnusedCallResult = false,
+                    reportAny = false,
+                    reportExplicitAny = false,
+                    reportUnusedExpression = false,
+                    reportMissingTypeArgument = false,
+                    reportUntypedFunctionDecorator = false,
+                }
+            }
+        }
+    }
+})
+vim.lsp.config("clangd", {
+    cmd = { "clangd", "--compile-commands-dir=/home/justin" },
+    filetypes = { "c", "cpp", "cc", "ixx", "cuda", "cu", "objcpp", "objc" },
+})
+vim.lsp.config("rust_analyzer", {})
+
+
+lspconfig.ts_ls.setup({
+    root_dir = Root_pattern_exclude({
+        root = { "package.json" },
+        exclude = { "deno.json", "deno.jsonc" }
+    }),
+    single_file_support = false,
+    settings = {
+        javascript = {
+            suggest = {
+                autoImports = true,
+            },
+            diagnostics = {
+                ignore = {"[Deno]"},
+            },
+        },
+        typescript = {
+            suggest = {
+                autoImports = true,
+            },
+            diagnostics = {
+                ignore = {"[Deno]"},
+            },
+            tsserverFilePaths = { "path/to/deno" },
+        },
+    },
+})
+
+
+lspconfig.denols.setup({
+    root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc", "deno.lock"),
+    init_options = {
+        lint = true,
+        suggest = {
+            imports = {
+                hosts = {
+                    ["https://deno.land"] = true,
+                },
+            },
+        },
+    },
+})
+
+-- tell autocomplete about neovim lsp completion
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(ev)
+        local client = vim.lsp.get_client_by_id(ev.data.client_id)
+        if client ~= nil and client:supports_method("textDocument/completion") then
+            vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = false })
+        end
+    end,
+})
--- lua/utils.lua
+++ lua/utils.lua
@@ -18,3 +18,22 @@
 --     local lfs = require("lfs")
 --     return lfs.currentdir() .. "/" .. path
 -- end
+
+
+---Specialized root pattern that allows for an exclusion
+---@param opt { root: string[], exclude: string[] }
+---@return fun(file_name: string): string | nil
+function Root_pattern_exclude(opt)
+  local lsputil = require('lspconfig.util')
+
+  return function(fname)
+    local excluded_root = lsputil.root_pattern(opt.exclude)(fname)
+    local included_root = lsputil.root_pattern(opt.root)(fname)
+
+    if excluded_root then
+      return nil
+    else
+      return included_root
+    end
+  end
+end
