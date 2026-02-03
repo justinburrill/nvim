b1aed4e440aa2e65baac484a7220078ccd2bdf6b
Justin Burrill <me@justinburrill.ca>
2026-02-03 17:12:43 -0500

    Tue Feb  3 17:12:43 EST 2026

--- lua/lspsettings.lua
+++ lua/lspsettings.lua
@@ -12,6 +12,13 @@ lspconfig.opts = {
     }
 }
 
+vim.lsp.config("lua_ls", {
+    settings = {
+        diagnostics = { globals = { "vim" }, },
+        workspace = { library = vim.api.nvim_get_runtime_file("", true), }
+    }
+})
+
 vim.lsp.config("neocmake", {
     cmd = { "neocmakelsp", "stdio" }
 })
