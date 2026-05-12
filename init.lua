036bf7593539f76cccbbccc514a750ae4d70b10c
Justin Burrill <me@justinburrill.ca>
2026-05-12 18:31:13 -0400

    add which-key group for picking git changes

--- init.lua
+++ init.lua
@@ -159,6 +159,7 @@ whichkey.add({
     { "<leader>c", group = "code" },
     { "<leader>p", group = "Pick" },
     { "<leader>L", group = "LSP commands" },
+    { "<leader>pC", group = "Git changes" },
 })
 
 -- TREESITTER
